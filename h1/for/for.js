a727724f42d2a728b5d0f49b0f3e087c79b5b987
ADI-SEMBIRING <edlisedy@gmail.com>
2020-10-27 16:18:19 +0700

    if dalam for

---
+++ h1/for/for.js
@@ -0,0 +1,12 @@
+var nomorAngkot=1;
+var angkotBeroperasi= 6;
+var jumlahAngkot =10;
+
+while( nomorAngkot<=angkotBeroperasi){
+      console.log( 'angkot no. '+ nomorAngkot + ' berjalan dengan baik')
+      nomorAngkot++;
+}
+
+for( angkotBeroperasi;  angkotBeroperasi<jumlahAngkot; angkotBeroperasi++){
+      console.log("angkot no. "+( angkotBeroperasi +1) + ' sedang rusak')
+}
---
+++ h1/for/script.js
@@ -0,0 +1,12 @@
+var a =1;
+var b =6;
+var c =10;
+
+for( a; a<=c; a++) {
+if(a<=b ){
+console.log('angkot no. '+a+' beroperasi dengan baik')
+}else {
+    console.log( 'angkot no, '+a+' sedang tidak beroperasi')
+}
+      
+}
