8e7906464adde31caf0ae27947e9db775067aad9
ADI-SEMBIRING <edlisedy@gmail.com>
2020-10-28 09:37:45 +0700

    elseIf

---
+++ elseIF.js
@@ -0,0 +1,11 @@
+var a= prompt('masukkan angka')
+ if(a % 2===0){
+       alert ( a+'  adalah bilangan genap')
+ }
+ else if (a % 2 === 1){
+  alert(a +' adalah bilangan ganjil')
+}
+ else{
+       alert( a+' bukan angka!')
+ } 
+ 
--- h1/for/script.js
+++ h1/for/script.js
@@ -3,10 +3,19 @@ var b =6;
 var c =10;
 
 for( a; a<=c; a++) {
-if(a<=b ){
+
+if(a<=b && a !==5 ) {
 console.log('angkot no. '+a+' beroperasi dengan baik')
-}else {
-    console.log( 'angkot no, '+a+' sedang tidak beroperasi')
+}
+
+
+else if (a ===8 || a===10 || a=== 5) {
+    console.log( 'angkot no, '+a+' sedang lembur')
+}
+
+
+else{
+    console.log('angkot no. '+ a+ ' sedang tidak beroperasi')
 }
       
 }
