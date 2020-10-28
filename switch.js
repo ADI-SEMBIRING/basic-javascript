6969db1b9cb577669f2f1108dd935de5d6c92637
ADI-SEMBIRING <edlisedy@gmail.com>
2020-10-28 14:04:04 +0700

    belajar switch

---
+++ switch.js
@@ -0,0 +1,21 @@
+var kendaraan=prompt('masukkan nama kendaraan')
+switch(kendaraan){
+      case 'mobil':
+      case 'traktor':
+      case 'truck':
+      case 'sepeda motor':
+      alert('anda memasukkan kendaraan darat!')
+      break;
+      case 'speedboat':
+      case 'ferry':
+      case 'tanker':
+      alert ('anda memasukkan transportasi laut')
+      break;
+      case 'pesawat terbang':
+      case 'jet':
+      case 'helikopter':
+      alert('transportasi udara')
+      break;
+      default:('kendaraan tidak dikenal')
+      break;
+}
