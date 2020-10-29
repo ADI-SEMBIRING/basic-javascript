dbdf4bbf3221f17e7ddbba5298aec1a12e509344
ADI-SEMBIRING <edlisedy@gmail.com>
2020-10-29 18:31:31 +0700

    fungsi1

---
+++ fungsi.js
@@ -0,0 +1,17 @@
+
+function jumlahVolumeKubus(a,b) {
+      
+
+var volumeA;
+var volumeB;
+var total;
+
+
+volumeA=a*a*a;
+volumeB=b*b*b;
+
+total= volumeA+volumeB
+
+return total;
+}
+alert(jumlahVolumeKubus(3,8));
