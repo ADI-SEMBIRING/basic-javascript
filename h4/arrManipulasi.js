6c5f4500b9d5e855f38d45da152ee02fec8b8671
ADI-SEMBIRING <edlisedy@gmail.com>
2020-10-30 13:52:35 +0700

    h1-h3

---
+++ h4/arrManipulasi.js
@@ -0,0 +1,18 @@
+//manipulasi array
+
+
+//menambah isi array
+// var arr=['a',1,true]
+// console.log (arr);
+
+//menghaous elemen array
+// var arr=["mangga","rambtan","pisang"];
+// arr[1]=undefined;
+// console.log(arr)
+
+//menampilkan seluruh isi array
+
+var arr=["pisang","mangga","durian","jambu"];
+for(var i=0;i<arr.length;i++){
+      console.log('buah ke-' +  (i+1)+ ' : ' + arr[i]);
+}
