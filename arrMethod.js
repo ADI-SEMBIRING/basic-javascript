cb0ff47d00232368cb6e64f27231aea5ec686f6d
ADI-SEMBIRING <edlisedy@gmail.com>
2020-10-31 13:05:21 +0700

    arrMethod

---
+++ arrMethod.js
@@ -0,0 +1,30 @@
+//JOIN 
+var arr = ['pisang', 'mangga','durian'];
+// console.log(arr.join ('-'));
+//pemisah dapat di ganti koma,penghubung spasi dll,
+
+//PUSH 
+//  arr.push('duku');
+ //duku akan masuk ke array  variabel diatas
+
+// //POP
+// arr.pop();
+//  console.log(arr.join ('-'));
+
+
+ //SHIFT dan UNSHIFT
+ //cara kerja sama dengan POPO dan  PUSH tetapi berjalan dari depan
+
+ //splice 
+ //menambah di tengah,dapt juga menghapuss
+
+//splice(index awal,maudihapusBerapa,elemenBaru,elemenBaru2,....);
+// arr.splice(2,0,'salak');
+//   console.log(arr.join ('-'));
+
+//forEach
+var angka=[1,2,3,4,5,6,7,8,];
+angka.forEach(function (e) {
+      console.log(e);
+      
+});
